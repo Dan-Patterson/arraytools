971660be5ca2d5e921442ddcddccc9e09ca344ca
Dan Patterson <Dan-Patterson@users.noreply.github.com>
2017-07-11 13:49:00 -0400

    Add files via upload

--- arraytools/analysis/__init__.py
+++ arraytools/analysis/__init__.py
@@ -2,3 +2,8 @@ from .compass_angles import compass
 from .line_ang_azim import line_dir
 from .near import not_closer, n_near
 from .vincenty import vincenty
+__all__ = ['compass',
+           'line_dir',
+		   'not_closer',
+		   'n_near',
+		   'vincenty']
