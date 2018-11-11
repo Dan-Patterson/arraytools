7d7b1ba58dcc730f203fad213d9a6a56da973306
Dan Patterson <Dan-Patterson@users.noreply.github.com>
2018-11-11 18:03:58 -0500

    Update __init__.py

--- arraytools/analysis/__init__.py
+++ arraytools/analysis/__init__.py
@@ -1,9 +1,13 @@
+from .arr_moving import block, stride_, rolling_stats
 from .compass_angles import compass
 from .line_ang_azim import line_dir
 from .near import not_closer, n_near
 from .vincenty import vincenty
-__all__ = ['compass',
+__all__ = ['block',
+           'stride_',
+           'rolling_stats',
+           'compass',
            'line_dir',
-		   'not_closer',
-		   'n_near',
-		   'vincenty']
+           'not_closer',
+	       'n_near',
+	       'vincenty']
