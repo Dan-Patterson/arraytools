2f97a810a631a6e62dd308d732e68b2d3117fdef
Dan Patterson <Dan-Patterson@users.noreply.github.com>
2019-03-17 13:12:51 -0400

    Add files via upload

--- arraytools/graphing/arr_scatter.py
+++ arraytools/graphing/arr_scatter.py
@@ -1,24 +1,45 @@
 # -*- coding: UTF-8 -*-
 """
-:Script:   arr_scatter.py
-:Author:   Dan.Patterson@carleton.ca
-:Modified: 2017-01-23
-:
-:Purpose:
-:
-:Notes:
-:
-:References:
-:
-:---------------------------------------------------------------------:
+===========
+arr_scatter
+===========
+
+Script : arr_scatter.py
+
+Author : Dan_Patterson@carleton.ca
+
+Modified : 2019-03-07
+
+Purpose:
+--------
+    Sample scatterplot plotting, in 2D and 3D
+
+Notes
+-----
+    >>> print(plt.style.available)
+    >>> import matplotlib.pyplot.figure as fig
+    # figure(num=None, figsize=None, dpi=None, facecolor=None, edgecolor=None,
+    #        frameon=True, FigureClass=<class 'matplotlib.figure.Figure'>,
+    #        clear=False, **kwargs)
+    # matplotlib.pyplot.subplots
+    # subplots(nrows=1, ncols=1, sharex=False, sharey=False, squeeze=True,
+    #          subplot_kw=None, gridspec_kw=None, **fig_kw)
+
+References
+----------
+
+`<https://matplotlib.org/users/customizing.html>`_.
+
+`<https://matplotlib.org/api/_as_gen/matplotlib.pyplot.figure.html>`_.
+
+`<https://matplotlib.org/api/_as_gen/matplotlib.pyplot.subplots.html>`_.
+
 """
 # ---- imports, formats, constants ----
 
 import sys
 import numpy as np
-import matplotlib
-#matplotlib.use('TkAgg', warn=True, force=False)  # pythonwin use TkAgg,
-matplotlib.use('QT5agg')  # don't work Agg, WX, QTAgg, QT4Agg
+# import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.markers import MarkerStyle
 
@@ -31,112 +52,139 @@ np.ma.masked_print_option.set_display('-')
 
 script = sys.argv[0]
 
+__all__ = ['plot_2d', 'plot_3d']
+
 # ---- functions ----
-
-
-# noinspection PyDefaultArgument,PyShadowingNames,PyShadowingNames
-def scatter_params(plt, fig, ax, title="Title", lbls=['X', 'Y']):
-    """Default parameters for plots
-    :Notes:
-    :  ticklabel_format(useoffset), turns off scientific formatting
-    """
-    x_label, y_label = lbls
-    font1 = {'family': 'sans-serif', 'color': 'black',
-             'weight': 'bold', 'size': 8}  # set size to other values
-    markers = ['o', 's', '+', '*', 'x']
-    ax.set_aspect('equal', adjustable='box')
-#    ax.margins(0.05)  # set a gap
-    ax.ticklabel_format(style='sci', axis='both', useOffset=False)
-    ax.set_xlabel(x_label, labelpad=12)
-    ax.xaxis.label_position = 'bottom'
-    ax.xaxis.label.set_fontsize(12)
-#    ax.get_xaxis().get_major_formatter().set_scientific(False)
-    ax.set_ylabel(y_label, labelpad=12)
-    ax.yaxis.label_position = 'left'
-    ax.yaxis.label.set_fontsize(12)
-    plt.title(title + "\n", loc='center', fontdict=font1, size=12)
-    plt.tight_layout
-    plt.grid(True)
-    return plt, fig, ax
-
-
-# noinspection PyDefaultArgument,PyShadowingNames,PyShadowingNames,
-# PyShadowingNames,PyShadowingNames
-def plot_pnts_(pnts, title='Title', r_c=False, lbls=['X', 'Y'], params=True):
+#
+def plot_2d(pnts, title='Title', r_c=False, ax_lbls=None, pnt_labels=True):
     """Plot points for Nx2 array representing x,y or row,col data.
-    :Requires:  see _params() to specify special parameters
-    :--------
-    :  pnts - point or row/column array
-    :  r_c - boolean. If True, the y-axis is inverted to represent row-column
-    :    formatting rather than x,y formatting.
-    :Returns:
-    :-------
-    :  A scatterplot representing the data.  It is easier to modify the
-    :  script below than to provide a whole load of input parameters.
-    :----------
+    
+    Parameters
+    ----------
+    see _params() to specify special parameters
+
+    pnts : array-like
+        2D array of point-like objects ie a row/column array
+    r_c : boolean
+        If True, the y-axis is inverted to represent row-column formatting
+        rather than x,y formatting.
+    ax_lbls : list
+        A list, like ['X', 'Y'] is needed, if left to `None` then that will
+        be the default.
+
+    Returns
+    -------
+    A scatterplot representing the data.  It is easier to modify the
+    script below than to provide a whole load of input parameters.
+
     """
-    from matplotlib.markers import MarkerStyle
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots(1, 1)
-    markers = MarkerStyle.filled_markers
-    # ---- optional parameter def ----
-    if params:  # use parameter def
-        plt, fig, ax = scatter_params(plt, fig, ax, title, lbls)
-        x_min, y_min = pnts.min(axis=0) - [0.5, 0.5]
-        x_max, y_max = pnts.max(axis=0) + [0.5, 0.5]
-        #
+    def scatter_params(plt, fig, ax, title="Title", ax_lbls=None):
+        """Default parameters for plots
+        :Notes:
+        :  ticklabel_format(useoffset), turns off scientific formatting
+        """
+        fig.set_figheight = 8
+        fig.set_figwidth = 8
+        fig.dpi = 200
+        if ax_lbls is None:
+            ax_lbls = ['X', 'Y']
+        x_label, y_label = ax_lbls
+        font1 = {'family': 'sans-serif', 'color': 'black',
+                 'weight': 'bold', 'size': 14}  # set size to other values
+        ax.set_aspect('equal', adjustable='box')
+        ax.ticklabel_format(style='sci', axis='both', useOffset=False)
+        ax.xaxis.label_position = 'bottom'
+        ax.yaxis.label_position = 'left'
+        plt.xlabel(x_label, fontdict=font1, labelpad=12, size=14)
+        plt.ylabel(y_label, fontdict=font1, labelpad=12, size=14)
+        plt.title(title + "\n", loc='center', fontdict=font1, size=20)
+        plt.tight_layout(pad=0.2, h_pad=0.1, w_pad=0.1)
+        plt.grid(True)
+        return
+    #
+    def label_pnts(pnts, plt):
+        """as it says"""       
         lbl = np.arange(len(pnts))
         for label, xpt, ypt in zip(lbl, pnts[:, 0], pnts[:, 1]):
-            plt.annotate(label, xy=(xpt, ypt), xytext=(2, 2), size=8,
-                         textcoords='offset points', ha='left', va='bottom')
-        plt.xlim(x_min, x_max)
-        if r_c:
-            plt.ylim(y_max, y_min)
-        else:
-            plt.ylim(y_min, y_max)
+            plt.annotate(label, xy=(xpt, ypt), xytext=(2, 2), size=12,
+                        textcoords='offset points', ha='left', va='bottom')
+        return
+    # ---- main plotting routine
+    fig, ax = plt.subplots(1, 1)
+    ax.set_aspect('equal', adjustable='box')
+    markers = MarkerStyle.filled_markers
+    # ---- set basic parameters ----
+    scatter_params(plt, fig, ax, title, ax_lbls)
+    if isinstance(pnts, (list, tuple)):
+        mn = np.min([i.min(axis=0) for i in pnts], axis=0) - [0.5, 0.5]
+        mx = np.max([i.max(axis=0) for i in pnts], axis=0 )+ [0.5, 0.5]
+        x_min, y_min = np.floor(mn)
+        x_max, y_max = np.ceil(mx)
+    else:
+        x_min, y_min = np.floor(pnts.min(axis=0) - [0.5, 0.5])
+        x_max, y_max = np.ceil(pnts.max(axis=0) + [0.5, 0.5])
+    # 
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    if r_c:
+        plt.ylim(y_max, y_min)
     # ---- enable multiple point files ----
     if isinstance(pnts, (list, tuple)):
-        i = 0
-        for p in pnts:  # plot x, y using marker i.
+        for i, p in enumerate(pnts):  # plot x, y using marker i.
             plt.scatter(p[:, 0], p[:, 1], marker=markers[i])
-            i += 1
+            if pnt_labels:
+                label_pnts(p, plt)
     else:
         plt.scatter(pnts[:, 0], pnts[:, 1])  # , marker=markers[0])
+        if pnt_labels:
+            label_pnts(pnts, plt)
+#    plt.ion()
     plt.show()
-#    plt.close()  # turn close back on in some IDEs
-    return plt, ax
 
 
-# ----------------------------------------------------------------------
-# .... running script or testing code section
-def _tool():
-    """run when script is from a tool
+def plot_3d(a):
+    """Plot an xyz sequence in 3d
+
+    Parameters
+    ----------
+    a : array-like
+        A 3D array of point objects representing X,Y and Z values
+
+    References
+    ----------
+    `<https://matplotlib.org/tutorials/toolkits/mplot3d.html#sphx-glr-
+    tutorials-toolkits-mplot3d-py>`_.
+
+    Example
+    -------
+    >>> x = np.arange(10)
+    >>> y = np.arange(10)
+    >>> z = np.array([5,4,3,2,1,1,2,3,4,5])
+    >>> xyz = np.c_[(x,y,z)]
+    >>> plot_3d(xyz)
     """
-    in_fc = sys.argv[1]
-    group_by = str(sys.argv[2])
-    x_fld = int(sys.argv[3])
-    y_fld = str(sys.argv[4])
-#    out_type = str(sys.argv[5])
-#    out_fc = sys.argv[6]
-    return in_fc, x_fld, y_fld
+    import matplotlib as mpl
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    #
+    mpl.rcParams['legend.fontsize'] = 10
+    #
+    fig = plt.figure()
+    fig.set_figheight = 8
+    fig.set_figwidth = 8
+    fig.dpi = 200
+    ax = Axes3D(fig)  # old  #ax = fig.gca(projection='3d')
+    #
+    x = a[:, 0]
+    y = a[:, 1]
+    z = a[:, 2]
+    ax.plot(x, y, z, label='xyz')
+    ax.legend()
+    #plt.tight_layout(pad=0.2, h_pad=0.1, w_pad=0.1)
+    plt.show()
 
-
-gdb_pth = "/".join(script.split("/")[:-2]) + "/Data/Point_tools.gdb"
-
-if len(sys.argv) == 1:
-    testing = True
-    in_fc = gdb_pth + r"/r_sorted"
-    group_by = 'Group_'
-    k_factor = 3
-    hull_type = 'concave'  # 'convex'
-    out_type = 'Polyline'
-    out_fc = gdb_pth + r"/r_11"
-else:
-    testing = False
-    in_fc, group_by, k_factor, hull_type, out_type, out_fc = _tool()
-
-
-# noinspection PyShadowingNames,PyShadowingNames,PyShadowingNames
+# ----------------------------------------------------------------------------
+# ---- running script or testing code section ----
 def _demo():
     """Plot 20 points which have a minimum 1 unit point spacing
     :
@@ -146,10 +194,9 @@ def _demo():
                   [5.3, 9.5], [5.5, 5.7], [6.1, 4.0], [6.5, 6.8],
                   [7.1, 7.6], [7.3, 2.0], [7.4, 1.0], [7.7, 9.6],
                   [8.5, 6.5], [9.0, 4.7], [9.6, 1.6], [9.7, 9.6]])
-    plt, ax = plot_pnts_(a, title='Points no closer than... test',
-                         r_c=False, lbls=['X-values', 'Y-values'],
-                         params=True)
-    return a, plt,  ax
+    plot_2d(a, title='Points no closer than... test',
+               r_c=False, ax_lbls=None, pnt_labels=True)
+    return a
 
 # ---------------------------------------------------------------------
 if __name__ == "__main__":
